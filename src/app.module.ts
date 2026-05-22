e17f750f28d8f663581636f34ba24f9126d75144
Carlos Daniel <carlosdaniel@hpeautos.com.br>
2026-05-22 13:41:31 -0300

    Initial: Commit inicial

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { StoreModule } from './store/store.module';
+
+@Module({
+  imports: [UsersModule, PrismaModule, StoreModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
